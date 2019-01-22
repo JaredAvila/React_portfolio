19d6c0922631c5ecaa9b94521dc51caf250749a1
Jared Avila <jared.c.avila@gmail.com>
2019-01-22 11:42:53 -0800

    updated images and text and added fontawesome icons.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,61 +7,71 @@ import Background_3 from "../static/business.jpeg";
 class Home extends Component {
   render() {
     return (
-      <section class="slider container">
-        <ul class="slides">
-          <li>
-            <img src={Background_1} alt="" />
-            <div class="caption center-align">
-              <h2>Serving the Silicon Valley</h2>
-              <h5 class="hide-on-small-only">
-                The goal of my teaching is to impart to my students an
-                appreciation, understanding and a love for music.
-              </h5>
-              <a
-                href="#contact"
-                class="btn btn-large teal waves-effect waves-light"
-              >
-                Contact Me
-              </a>
-            </div>
-          </li>
-          <li>
-            <img src={Background_2} alt="" />
-            <div class="caption left-align">
-              <h2>Solid Foundation</h2>
-              <h5 class="light grey-text text-lighten-3 hide-on-small-only">
-                Whether the student is a beginner or a more advanced player,
-                musical skills and technique must be built on a “solid
-                foundation” to achieve musical mastery in every technical aspect
-                and at every playing level.
-              </h5>
-              <a
-                href="/projects"
-                class="btn btn-large deep-purple waves-effect waves-red"
-              >
-                See Projects
-              </a>
-            </div>
-          </li>
-          <li>
-            <img src={Background_3} alt="" />
-            <div class="caption right-align">
-              <h2>Years of Professional Experience</h2>
-              <h5 class="light grey-text text-lighten-3 hide-on-small-only">
-                Utilizing my “solid foundation” method, my students are given
-                the best opportunity to be successful in their musical training
-                and development.
-              </h5>
-              <a
-                href="/about"
-                class="btn btn-large deep-orange waves-effect waves-light"
-              >
-                Learn More
-              </a>
-            </div>
-          </li>
-        </ul>
-      </section>
+      <div>
+        <section class="slider">
+          <ul class="slides">
+            <li>
+              <img src={Background_1} alt="" />
+              <div class="caption center-align">
+                <h2>Modern Responsive Design</h2>
+                <h5 class="hide-on-small-only">
+                  Our goal is to deliver beautiful, fully responsive,
+                  interactive, unique and modern products and services that will
+                  engage clients/customers.
+                </h5>
+                <a
+                  href="#contact"
+                  class="btn btn-large pink accent-3 waves-effect waves-light"
+                >
+                  Free quote
+                </a>
+              </div>
+            </li>
+            <li>
+              <img src={Background_2} alt="" />
+              <div class="caption left-align">
+                <h2>Experience Speaks</h2>
+                <h5 class="light grey-text text-lighten-3 hide-on-small-only">
+                  "This will be someones testimonial when I eventually get one.
+                  When I do it will be the best! Woo hoo!"
+                  <br />
+                  <br /> -Person McPeoplson
+                </h5>
+                <a
+                  href="/projects"
+                  class="btn btn-large deep-purple waves-effect waves-red"
+                >
+                  More Reviews
+                </a>
+              </div>
+            </li>
+            <li>
+              <img src={Background_3} alt="" />
+              <div class="caption right-align">
+                <h2>Seach Engine Optimization</h2>
+                <h5 class="light grey-text text-lighten-3 hide-on-small-only">
+                  Get your website on the radar! We offer more than just a
+                  webpage. Our Web Services packages offer "SEO" (Serach Engine
+                  Optimiztaion) friendly sites, social media mangement,
+                  marketing and more!
+                </h5>
+                <a
+                  href="/services"
+                  class="btn btn-large deep-orange waves-effect waves-light"
+                >
+                  Learn More
+                </a>
+              </div>
+            </li>
+          </ul>
+        </section>
+        <section className="section">
+          <i class="fas fa-chart-pie fa-4x" />
+          <i class="fas fa-chart-line fa-4x" />
+          <i class="fas fa-search-dollar fa-4x" />
+          <i class="fas fa-sitemap fa-4x" />
+        </section>
+      </div>
     );
   }
 }
